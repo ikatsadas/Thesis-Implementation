be778e30a38413e0f750cf39cdeb1f3032880936
ikatsadas <johnkats5896@gmail.com>
2018-04-25 03:17:43 +0200

    ADDED: graph of tweets retweeted by verified users

--- plotting.py
+++ plotting.py
@@ -10,7 +10,7 @@ import numpy as np
 # uri = "mongodb://user:SpYZ7EMlgs@snf-795686.vm.okeanos.grnet.gr:25"
 client = MongoClient('localhost', 27017)
 # client = MongoClient(uri)
-db = client['getMostRetweetedFake']
+db = client['getMostRetweetedTest']
 
 mins=30# number of minutes for the time interval
 
@@ -215,5 +215,98 @@ def individual_fugures():
     plt.suptitle("Propagation of FAKE news tweets")
     plt.show()
 
-overall_figure()
+
+def graphs_of_verified_users():
+    f, axarr = plt.subplots(2, 5, sharex=True, sharey=True)
+    matplotlib.rcParams.update({'font.size': 5})
+    c = 0
+    for og_tweet_collection in db.collection_names():  # for every tweet
+        collection = db[og_tweet_collection]
+        cursor = collection.find({})  # Gets the tweets in that topic
+        retweet_time = []
+        flag = False
+        for document in cursor:
+            # if this retweet is from a verified user print YES
+            if document["user"]["verified"]:
+                print document["user"]["verified"], "----", og_tweet_collection, "----", document["user"]["id"]
+                flag = True
+                s = document["created_at"]
+                dt = datetime.now()
+                dt = dt.strptime(s, '%a %b %d %H:%M:%S +0000 %Y')
+                retweet_time.append(dt)
+        if flag:
+            c = c + 1
+            print c
+            if c > 10:
+                break
+            # make a graph for this tweet
+            if len(retweet_time) != 0:
+                retweet_time = sorted(retweet_time)
+                start_date = retweet_time[0]
+                end_date = retweet_time[-1]
+                c_time = start_date
+                x = []
+                it = 0
+                while (c_time < end_date):
+                    c_time = start_date + timedelta(minutes=mins * it)
+                    x.append(c_time)
+                    it = it + 1
+                s = x[0]
+                e = x[1]
+                pointer = 0
+                values = []
+                for i in x:
+                    values.append(0)
+                for i in retweet_time:
+                    if i > e:
+                        flag = True
+                        while flag:
+                            pointer = pointer + 1
+                            if pointer > len(x) - 1:
+                                break
+                            s = x[pointer]
+                            e = x[pointer + 1]
+                            if e >= i >= s:
+                                flag = False
+                    if e >= i >= s:
+                        values[pointer] = values[pointer] + 1
+
+                counter = 0
+                for i in values:
+                    values[counter] = i / float(len(retweet_time))
+                    counter = counter + 1
+                dates = matplotlib.dates.date2num(x)
+                if c <= 5:
+                    k = 0
+                    l = c - 1
+                else:
+                    k = 1
+                    l = c - 6
+                # axarr[k, l].plot_date(dates, values, 'r--')
+                # trim the list to a smaller time interval
+                if (len(values) > 30):
+                    values = values[:30]
+                axarr[k, l].plot(values, 'r')
+                axarr[k, l].set_title(og_tweet_collection)
+                # plt.subplot(2, 5, c)
+                # plt.xticks(rotation=30)
+                # plt.ylabel('Frequency of retweets')
+                # plt.title(str(c))#tweet ID
+                # plt.plot_date(dates, values, 'r--')
+                plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.1, hspace=0.2)
+            else:
+                c = c - 1
+    for ax in axarr.flat:
+        ax.set(xlabel='# of 30 mins', ylabel='Frequency of retweets')
+        ax.label_outer()
+    for ax in f.axes:
+        matplotlib.pyplot.sca(ax)
+        plt.xticks(rotation=30)
+    plt.suptitle("Propagation of FAKE news tweets")
+    plt.show()
+
+# overall_figure()
 # individual_fugures()
+# graphs_of_verified_users()
+
+
